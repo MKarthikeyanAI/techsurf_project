d1fd2148541f281e759339ab0c7658b067ac2419
MKarthikeyanAI <mkarthikeyan00100010@gmail.com>
2024-10-21 15:20:10 +0530

    contenttype changed

--- OneDrive/Desktop/geminiapi_project/my-app/src/components/RightSidebar.js
+++ OneDrive/Desktop/geminiapi_project/my-app/src/components/RightSidebar.js
@@ -3,6 +3,14 @@ import './RightSidebar.css';
 import ContentTypeCard from './ContentTypeCard';
 
 const RightSidebar = ({ contentTypes }) => {
+  // Check if contentTypes is null, undefined, or an empty array
+  if (!contentTypes || contentTypes.length === 0) {
+    return (
+      <div className="right-sidebar">
+        <h2>No ContentTypes Available...</h2>
+      </div>
+    );
+  }
   return (
     <div className="right-sidebar">
       <h2>Content Types</h2>
@@ -15,7 +23,7 @@ const RightSidebar = ({ contentTypes }) => {
         ))}
       </div>
     </div>
-  );
+  );  
 };
 
 export default RightSidebar;
